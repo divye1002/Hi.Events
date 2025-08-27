bdbc111d2ac0146be7b606ab9545df5d904fbcde
divey1002 <divyenatani1002@gmail.com>
2025-08-27 18:48:27 +0530

    promo removal

--- frontend/src/components/routes/product-widget/SelectProducts/index.tsx
+++ frontend/src/components/routes/product-widget/SelectProducts/index.tsx
@@ -536,12 +536,6 @@ const SelectProducts = (props: SelectProductsProps) => {
                 </form>
             )}
             <div className={'hi-promo-code-row'}>
-                {(!showPromoCodeInput && !form.values.promo_code) && (
-                    <Anchor className={'hi-have-a-promo-code-link'}
-                            onClick={() => setShowPromoCodeInput(true)}>
-                        {t`Have a promo code?`}
-                    </Anchor>
-                )}
                 {form.values.promo_code && (
                     <div className={'hi-promo-code-applied'}>
                         <span><b>{form.values.promo_code}</b> {t`applied`}</span>
